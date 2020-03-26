3b81861a1998ed66da4549cbd3ef8821b106d122
dmitrii1600 <shevchenko.dmitrii2000@gmail.com>
2020-03-26 16:49:19 +0200

    components part 1

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from "reactstrap/es/Navbar";
 import {NavbarBrand} from "reactstrap";
+import Menu from "./components/MenuComponent";
 
 class App extends Component {
     render() {
         return (
-            <div className="App">
+            <div >
                 <Navbar dark color='primary'>
                     <div className='container'>
                         <NavbarBrand href='#'> Nav Bar Confusion</NavbarBrand>
                     </div>
                 </Navbar>
+                <Menu/>
             </div>
         );
     }
