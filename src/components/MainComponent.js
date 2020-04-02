54cd5091a21ff34b0d71281b3d669ba7260a5277
dmitrii1600 <shevchenko.dmitrii2000@gmail.com>
2020-04-02 17:36:24 +0300

    "SPA part 1"

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,32 +1,43 @@
 import React, {Component} from 'react';
 import {DISHES} from "../shared/dishes";
+import {LEADERS} from "../shared/leaders";
+import {PROMOTIONS} from "../shared/promotions";
+import {COMMENTS} from "../shared/comments";
 import Menu from "./MenuComponent";
 import Header from "./HeaderComponent";
 import Footer from "./FooterComponent";
 import {Redirect, Route, Switch} from "react-router-dom";
 import Home from "./HomeComponent";
+import Contact from "./ContactComponent";
 
 class Main extends Component {
     constructor(props) {
         super(props);
         this.state = {
             dishes: DISHES,
+            promotions: PROMOTIONS,
+            leaders: LEADERS,
+            comments: COMMENTS,
         };
     }
 
     render() {
-        const HomePage = () => (<Home/>)
+        const HomePage = () => {
+            return(
+                <Home
+                    dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+                    promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+                    leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+                />
+            );
+        };
         return (
             <div>
                 <Header/>
-               {/* <Menu dishes={this.state.dishes}
-                      onClick={(dishId) => this.onDishSelect(dishId)}
-                />
-                <DishDetailComponent dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]}/>
-               */}
                 <Switch>
                     <Route path='/home' component={HomePage}/>
                     <Route exact path='/menu' component={() =>  <Menu dishes={this.state.dishes}/>}/>
+                    <Route exact path='/contactus' component={Contact}/>
                     <Redirect to='/home'/>
                 </Switch>
                <Footer/>
