2860ad09e2c1bef4ca629d454c7c0c733c8ea5f0
dmitrii1600 <shevchenko.dmitrii2000@gmail.com>
2020-04-10 14:08:01 +0300

    "Intro to Redux"

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,61 +1,68 @@
 import React, {Component} from 'react';
-import {DISHES} from "../shared/dishes";
-import {LEADERS} from "../shared/leaders";
-import {PROMOTIONS} from "../shared/promotions";
-import {COMMENTS} from "../shared/comments";
 import Menu from "./MenuComponent";
 import Header from "./HeaderComponent";
 import Footer from "./FooterComponent";
-import {Redirect, Route, Switch} from "react-router-dom";
+import {Redirect, Route, Switch, withRouter} from "react-router-dom";
 import Home from "./HomeComponent";
 import Contact from "./ContactComponent";
 import DishDetail from "./DishDetailComponent";
 import About from "./AboutComponent";
+import {connect} from 'react-redux';
+import {compose} from "redux";
+
+const mapStateToProps = state => {
+    return {
+        dishes: state.dishes,
+        comments: state.comments,
+        promotions: state.promotions,
+        leaders: state.leaders
+    }
+}
 
 class Main extends Component {
     constructor(props) {
         super(props);
-        this.state = {
-            dishes: DISHES,
-            promotions: PROMOTIONS,
-            leaders: LEADERS,
-            comments: COMMENTS,
-        };
+
     }
 
     render() {
         const HomePage = () => {
-            return(
+            return (
                 <Home
-                    dish={this.state.dishes.filter((dish) => dish.featured)[0]}
-                    promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
-                    leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+                    dish={this.props.dishes.filter((dish) => dish.featured)[0]}
+                    promotion={this.props.promotions.filter((promo) => promo.featured)[0]}
+                    leader={this.props.leaders.filter((leader) => leader.featured)[0]}
                 />
             );
-        };
+        }
 
         const DishWithId = ({match}) => {
-            return(
-                <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]}
-                            comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} />
+            return (
+                <DishDetail dish={this.props.dishes.filter((dish) => dish.id === parseInt(match.params.dishId, 10))[0]}
+                            comments={this.props.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId, 10))}/>
             );
         };
 
         return (
             <div>
                 <Header/>
-                <Switch>
-                    <Route path='/home' component={HomePage}/>
-                    <Route exact path='/menu' component={() =>  <Menu dishes={this.state.dishes}/>}/>
-                    <Route path='/menu/:dishId' component={DishWithId} />
-                    <Route exact path='/contactus' component={Contact}/>
-                    <Route exact path='/aboutus' component={() => <About leaders={this.state.leaders}/>}/>
-                    <Redirect to='/home'/>
-                </Switch>
-               <Footer/>
+                <div>
+                    <Switch>
+                        <Route path='/home' component={HomePage}/>
+                        <Route exact path='/aboutus' component={() => <About leaders={this.props.leaders}/>}/>} />
+                        <Route exact path='/menu' component={() => <Menu dishes={this.props.dishes}/>}/>
+                        <Route path='/menu/:dishId' component={DishWithId}/>
+                        <Route exact path='/contactus' component={Contact}/>} />
+                        <Redirect to="/home"/>
+                    </Switch>
+                </div>
+                <Footer/>
             </div>
         );
     }
 }
 
-export default Main;
+export default compose(
+    connect(mapStateToProps),
+    withRouter,
+)(Main);
